0cd91838fbe0214543505a70514235c9a7609f98
oreonoreon <oreonoreon@yandex.ru>
2022-12-03 09:16:28 +0300

    add Debug var to write out log to standart output or in file.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"CurrencyRateBot/internal/bot"
+	"CurrencyRateBot/internal/logger"
 )
 
+func init() {
+	logger.Debug = true
+}
 func main() {
 	bot.Bot()
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -7,11 +7,28 @@ import (
 )
 
 var Log *log.Logger
+var Debug bool
 
 func init() {
-	file, err := os.OpenFile("CurrencyBotLog.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
-	if err != nil {
-		panic(err)
+	var file *os.File
+	var err error
+	if Debug {
+		file = os.Stdout
+	} else {
+		if file, err = os.OpenFile("CurrencyBotLog.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755); err != nil {
+			file.Close()
+			panic(err)
+		}
 	}
 	Log = log.New(file, time.Now().String(), 0)
+}
+
+type checker interface {
+	Check()
+}
+type A struct {
+}
+
+func (a A) Check() {
+
 }
